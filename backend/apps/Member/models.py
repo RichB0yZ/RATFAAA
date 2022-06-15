3afdb5306eea5e6ab6c2e989994412d958f0fd30
Quilla <101010846+sirawitchair@users.noreply.github.com>
2022-06-15 10:24:09 +0700

    update models

--- backend/apps/Member/models.py
+++ backend/apps/Member/models.py
@@ -6,7 +6,7 @@ class Status(models.TextChoices):
     MILLITARY = 'MT', _('กลาโหม')
     RETIRE = 'RT', _('เกษียณ')
     RESIGN = 'RS', _('ลาออก')
-    DEATH = 'DT', _('ตาย')
+    DEATH = 'DT', _('เสียชีวิต')
 
 class Member(models.Model):
     
@@ -14,17 +14,19 @@ class Member(models.Model):
     title = models.CharField(verbose_name="คำนำหน้า", null=True, blank=True,max_length = 20)
     full_name = models.CharField(verbose_name = 'ชื่อ-นามสกุล', max_length = 150)
     position =  models.CharField(verbose_name="ตำแหน่ง", max_length = 100,null = True, blank=True)
+    corps =  models.CharField(verbose_name="เหล่า", max_length = 50,null = True, blank=True)
     unit =  models.CharField(verbose_name="หน่วยงาน", max_length = 50,null = True, blank=True)
     mobile  = models.CharField(verbose_name = 'เบอร์มือถือ', max_length = 20)
     status = models.CharField(verbose_name = 'สถานะ',max_length = 5, choices = Status.choices, default = Status.AIRFORCE, null = True, blank = True)
-
+    date_retire=models.DateField(verbose_name='วัน/เดือน/ปี ที่เกษียณ')
+    
     def __str__(self):
-        return f"{self.class_year} {self.title} {self.full_name} "
+        return f"{self.class_year} {self.title} {self.full_name}"
 
     class Meta:
-        verbose_name_plural = "สมาชิก"    
-        # ordering = ('-ActionOccure', 'ActionUser', 'ActionDo')
-
-
-
+        verbose_name_plural = "สมาชิก" 
+        
+class Event(models.Model):
+    
+    members=models.ManyToManyField(Member)
 
