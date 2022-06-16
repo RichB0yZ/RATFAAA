0f54044c6604efcbc525bf8ebf6bda0f3c8cfd8a
Quilla <101010846+sirawitchair@users.noreply.github.com>
2022-06-16 10:20:29 +0700

    models and fronend

--- backend/apps/Member/models.py
+++ backend/apps/Member/models.py
@@ -10,15 +10,16 @@ class Status(models.TextChoices):
 
 class Member(models.Model):
     
-    class_year = models.IntegerField(verbose_name = 'รุ่น', null=True, blank = True)
-    title = models.CharField(verbose_name="คำนำหน้า", null=True, blank=True,max_length = 20)
+    rank =  models.CharField(verbose_name="ยศ", max_length = 50,null = True, blank=True)
     full_name = models.CharField(verbose_name = 'ชื่อ-นามสกุล', max_length = 150)
-    position =  models.CharField(verbose_name="ตำแหน่ง", max_length = 100,null = True, blank=True)
+    mobile  = models.CharField(verbose_name = 'เบอร์มือถือ', max_length = 20)
     corps =  models.CharField(verbose_name="เหล่า", max_length = 50,null = True, blank=True)
     unit =  models.CharField(verbose_name="หน่วยงาน", max_length = 50,null = True, blank=True)
-    mobile  = models.CharField(verbose_name = 'เบอร์มือถือ', max_length = 20)
+    position =  models.CharField(verbose_name="ตำแหน่ง", max_length = 100,null = True, blank=True)
+    date_retire=models.DateField(verbose_name='วัน/เดือน/ปี ที่เกษียณ',null = True)
     status = models.CharField(verbose_name = 'สถานะ',max_length = 5, choices = Status.choices, default = Status.AIRFORCE, null = True, blank = True)
-    date_retire=models.DateField(verbose_name='วัน/เดือน/ปี ที่เกษียณ')
+    class_year_ntt= models.IntegerField(verbose_name = 'รุ่น นตท.', null=True, blank = True)
+    class_year_nno= models.IntegerField(verbose_name = 'รุ่น นนอ.', null=True, blank = True)
     
     def __str__(self):
         return f"{self.class_year} {self.title} {self.full_name}"
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,16 @@
+
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyData/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyData from './components/show_helloworld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MyData
+
   }
 }
 </script>
